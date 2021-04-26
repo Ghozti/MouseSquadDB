ad55945ca3a0e23379f4cd0a67a173292d4187d4
Ghozti <ybalderas1005@gmail.com>
2021-04-26 15:56:54 -0400

    - added constructors to Problem as well as fields

--- src/ghozti/mousesquad/structure/Problem.java
+++ src/ghozti/mousesquad/structure/Problem.java
@@ -1,4 +1,29 @@
 package ghozti.mousesquad.structure;
 
 public class Problem {
+
+    private String name, description, solution;
+
+    public Problem(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public Problem(String name, String description, String solution){
+        this.name = name;
+        this.description = description;
+        this.solution = solution;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getSolution(){
+        return solution;
+    }
 }

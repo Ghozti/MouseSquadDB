809e10b7a078c78fd4cda72241bdb5957d54183f
Ghozti <ybalderas1005@gmail.com>
2021-04-27 15:45:05 -0400

    - added more classes to Displayer

--- src/ghozti/mousesquad/structure/displayer/Displayer.java
+++ src/ghozti/mousesquad/structure/displayer/Displayer.java
@@ -4,7 +4,23 @@ abstract class Displayer {
 
     public abstract void displayProperties(String a, String b, String c);
 
-    public static class ProblemDisplayer extends Displayer{
+    public static class SolutionDisplayer extends Displayer {
+
+        @Override
+        public void displayProperties(String a, String b, String c) {
+
+        }
+    }
+
+    public static class UnresolvedProblemDisplayer extends Displayer {
+
+        @Override
+        public void displayProperties(String a, String b, String c) {
+
+        }
+    }
+
+    public static class peopleDisplayer extends Displayer {
 
         @Override
         public void displayProperties(String a, String b, String c) {

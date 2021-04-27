dde78a1f675ba1dc0ab4bd7d5b917007198eff17
Ghozti <ybalderas1005@gmail.com>
2021-04-27 16:06:30 -0400

    - finished Person

--- src/ghozti/mousesquad/structure/Person.java
+++ src/ghozti/mousesquad/structure/Person.java
@@ -1,4 +1,24 @@
 package ghozti.mousesquad.structure;
 
 public class Person {
+
+    private String name, emailOrPhone, osis;
+
+    public Person(String name, String emailOrPhone, String osis){
+        this.name = name;
+        this.emailOrPhone = emailOrPhone;
+        this.osis = osis;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmailOrPhone(){
+        return emailOrPhone;
+    }
+
+    public String getOsis(){
+        return osis;
+    }
 }
